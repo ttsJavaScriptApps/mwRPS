b64a769859ed66af3fd2ba256ddb7672184ccd07
Matthew <mw@matthewis.me>
2016-01-28 16:44:03 -0500

    rps 3

--- rps.js
+++ rps.js
@@ -1,4 +1,6 @@
 var hands = ['rock', 'paper', 'scissors'];
+var hand1;
+var hand2;
 var winner;
 
 var player1 = {
@@ -18,24 +20,22 @@ var player2 = {
 }
 
 
-
+// play round of RPS and determing winner based on hands played
 function playRound(argPlayerObj1, argPlayerObj2) {
-  var hand1 = argPlayerObj1.getHand();
-  var hand2 = argPlayerObj2.getHand();
+  hand1 = argPlayerObj1.getHand();
+  hand2 = argPlayerObj2.getHand();
   
-  if ( hand1 === hand2 ) {
-    console.log("It's a tie. Round: " + player1.name + "'s hand= " + hand1 + " & " + player2.name + "'s hand = " + hand2);
+  if (hand1 === hand2) {
+    console.log("It's a tie. Round: " + argPlayerObj1.name + "'s hand= " + hand1 + " & " + argPlayerObj2.name + "'s hand = " + hand2);
   } else {
-
     if ( (hand1 === "rock" && hand2 === "scissors") || (hand1 === "paper" && hand2 === "rock") || (hand1 === "scissors" && hand2 === "paper") ) {
-      winner = player1.name;
-      player1.wins++;
+      winner = argPlayerObj1.name;
+      argPlayerObj1.wins++;
     } else {
-      winner = player2.name;
-      player2.wins++;
+      winner = argPlayerObj2.name;
+      argPlayerObj2.wins++;
     }
-
-    console.log("Winner = " + winner + ", Round: " + player1.name + "'s hand= " + hand1 + " & " + player2.name + "'s hand = " + hand2);
+    console.log("Winner = " + winner + ", Round: " + argPlayerObj1.name + "'s hand= " + hand1 + " & " + argPlayerObj2.name + "'s hand = " + hand2);
     return winner;
   }
   
@@ -43,12 +43,10 @@ function playRound(argPlayerObj1, argPlayerObj2) {
 }
 
 
-
+// play game until winner decided
 function playGame(playerOne, playerTwo, playUntil) {
-  // figure it out
   while (playerOne.wins < playUntil && playerTwo.wins < playUntil) {
     playRound(playerOne, playerTwo);
-    // console.log("while");
   }
 
   if (playerOne.wins === playUntil) {
@@ -60,7 +58,7 @@ function playGame(playerOne, playerTwo, playUntil) {
 }
 
 
-
+// play tournament of RPS with 4 players
 function playTournament(playerOne, playerTwo, playerThree, playerFour, playUntil) {
   console.log("playTournament");
 }
